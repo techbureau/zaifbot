6db504e93b56d129d0337cb91c685f75c45d02a2
Tatsuki Monji <tatsuki@Tatsukis-MacBook-Air.local>
2017-02-13 16:49:04 +0100

    fix issue#31

--- zaifbot/modules/moving_average.py
+++ zaifbot/modules/moving_average.py
@@ -1,5 +1,6 @@
 from bot_common.db import ZaifbotDb
 
+
 class Tradelogs:
     _CREATE_TABLE = """
       CREATE TABLE IF NOT EXISTS {}
@@ -15,6 +16,34 @@ class Tradelogs:
       )
     """
 
+    _SELECT_COUNT = """
+        SELECT
+            COUNT(time)
+        FROM
+            {}
+        WHERE
+            time < ?
+            AND time > ?
+            AND closed = 1
+    """
+
+    _INSERT = """
+        INSERT OR IGNORE
+        INTO {}
+        (
+            time,
+            open,
+            high,
+            low,
+            close,
+            average,
+            volume,
+            closed
+        )
+        VALUES
+            (?,?,?,?,?,?,?,?)
+    """
+
     def __init__(self, currency_pair, period):
         self._instance = ZaifbotDb()
         self._table_name = 'tradelogs_{}_{}'.format(currency_pair, period)
@@ -24,8 +53,7 @@ class Tradelogs:
             self._CREATE_TABLE.format(self._table_name))
 
     def get_tradelogs_count(self, end_time, start_time):
-        query = 'SELECT COUNT(time) FROM {} WHERE time < ? AND time > ? AND closed = 1;'.format(
-            self._table_name)
+        query = self._SELECT_COUNT.format(self._table_name)
         params = (end_time, start_time)
         self._instance.cursor.execute(query, params)
         trade_logs_count = self._instance.cursor.fetchone()
@@ -36,14 +64,15 @@ class Tradelogs:
         insert_params = []
         update_params = []
         for i in tradelogs_api:
-            insert_params.append((i['time'], i['open'], i['high'], i['low'], i[
-                                 'close'], i['average'], i['volume'], int(i['closed'])))
-            update_params.append((i['open'], i['high'], i['low'], i['close'], i[
-                                 'average'], i['volume'], int(i['closed']), i['time']))
+            insert_params.append((i['time'], i['open'], i['high'], i['low'],
+                                  i['close'], i['average'], i['volume'],
+                                  int(i['closed'])))
+            update_params.append((i['open'], i['high'], i['low'], i['close'],
+                                  i['average'], i['volume'], int(i['closed']),
+                                  i['time']))
 
         # insert if missing
-        insert_query = 'INSERT OR IGNORE INTO {} (time, open, high, low, close, average, volume, closed) VALUES(?,?,?,?,?,?,?,?);'.format(
-            self._table_name)
+        insert_query = self._INSERT.format(self._table_name)
         self._instance.conn.executemany(insert_query, insert_params)
         self._instance.conn.commit()
 
@@ -56,6 +85,24 @@ class MovingAverage:
         sma REAL,
         ema REAL
       )
+    """
+
+    _SELECT = """
+        SELECT
+            T1.time, T1.close, T1.closed, T2.{}
+        FROM
+            {} AS T1 LEFT JOIN {} AS T2 ON T1.time = T2.time
+        WHERE
+            T1.time < ?
+            AND T1.time > ?
+    """
+
+    _INSERT = """
+        INSERT OR IGNORE
+        INTO {}
+            (time, {})
+        VALUES
+            (?,?)
     """
 
     def __init__(self, currency_pair, period, length, sma_ema):
@@ -71,8 +118,8 @@ class MovingAverage:
             self._CREATE_TABLE.format(self._mvavrg_table_name))
 
     def get_moving_average(self, end_time, start_time):
-        query = 'SELECT T1.time, T1.close, T1.closed, T2.{} FROM {} AS T1 LEFT JOIN {} AS T2 ON T1.time = T2.time WHERE T1.time < ? AND T1.time > ?;'.format(
-            self._sma_ema, self._trdlg_table_name, self._mvavrg_table_name)
+        query = self._SELECT.format(self._sma_ema, self._trdlg_table_name,
+                                    self._mvavrg_table_name)
         params = (end_time, start_time)
         self._instance.cursor.execute(query, params)
         moving_average = self._instance.cursor.fetchall()
@@ -80,7 +127,7 @@ class MovingAverage:
         return moving_average
 
     def update_moving_average(self, insert_params):
-        insert_query = 'INSERT OR IGNORE INTO {} (time, {}) VALUES(?,?);'.format(
-            self._mvavrg_table_name, self._sma_ema)
+        insert_query = self._INSERT.format(self._mvavrg_table_name,
+                                           self._sma_ema)
         self._instance.conn.executemany(insert_query, insert_params)
         self._instance.conn.commit()
