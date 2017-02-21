54c614220d9ca42120c8c427f0ae3d17d8fae434
taniguchi_akira <dededededaiou2003@yahoo.co.jp>
2017-02-21 17:55:07 +0900

    ちょっと修正

--- zaifbot/modules/auto_trade/__init__.py
+++ zaifbot/modules/auto_trade/__init__.py
@@ -3,3 +3,4 @@ from .additional_purchase import AdditionalPurchase
 from .custom import Custom
 
 __all__ = ['LossCut', 'AdditionalPurchase', 'Custom']
+
--- zaifbot/modules/auto_trade/process_common.py
+++ zaifbot/modules/auto_trade/process_common.py
@@ -5,18 +5,17 @@ from zaifbot.bot_common.config import load_config
 
 
 class ProcessBase(Process, metaclass=ABCMeta):
-    def __init__(self):
+    def __init__(self, func):
         super().__init__(name=self.get_name())
         self.config = load_config()
+        self._func = func
 
     def run(self):
         while True:
             sleep(self.config.system.sleep_time)
             if self.is_started() is False:
                 continue
-            stop_process_flg = self.execute()
-            if stop_process_flg:
-                break
+            self._func(self.config)
 
     @abstractmethod
     def get_name(self):
@@ -24,8 +23,4 @@ class ProcessBase(Process, metaclass=ABCMeta):
 
     @abstractmethod
     def is_started(self):
-        raise NotImplementedError
-
-    @abstractmethod
-    def execute(self):
         raise NotImplementedError
