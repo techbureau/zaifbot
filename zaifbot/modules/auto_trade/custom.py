1d8d5758029053ce1d6c408ea3154849b723ce31
taniguchi_akira <dededededaiou2003@yahoo.co.jp>
2017-02-22 09:51:29 +0900

    一部修正

--- zaifbot/modules/auto_trade/custom.py
+++ zaifbot/modules/auto_trade/custom.py
@@ -3,16 +3,13 @@ from zaifbot.modules.auto_trade.process_common import ProcessBase
 
 
 class Custom(ProcessBase):
-    def __init__(self, name, func, is_started):
-        super().__init__(func)
-        self._is_started = is_started
-        self._name = name
-
+    @abstractmethod
     def get_name(self):
-        return self._name
+        raise NotImplementedError
 
+    @abstractmethod
     def is_started(self):
-        return self._is_started(self.config)
+        raise NotImplementedError
 
     @abstractmethod
     def execute(self):
