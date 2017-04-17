056fa1ec6bb669c29bdcfdfdee69ee17e4ac7ca1
daikishiroi <d.shiroi@techbureau.jp>
2017-04-17 14:34:19 +0900

    make arg private

--- zaifbot/modules/processes/sell.py
+++ zaifbot/modules/processes/sell.py
@@ -5,14 +5,14 @@ from zaifbot.modules.processes.process_common import ProcessBase
 
 class SellByPrice(ProcessBase):
     def __init__(self, target_value):
-        self.target_value = target_value
+        self._target_value = target_value
 
     def get_name(self):
         return 'sell_by_price'
 
     def is_started(self):
         last_price = get_current_last_price()
-        if last_price >= self.target_value:
+        if last_price >= self._target_value:
             return True
         return False
 
