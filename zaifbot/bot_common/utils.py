c23f142fc293be258f58305f402d2eced7b67a74
Akira Taniguchi <a.taniguchi@techbureau.jp>
2017-04-20 11:40:06 +0900

    まだばぐってるけどコミット

--- zaifbot/bot_common/utils.py
+++ zaifbot/bot_common/utils.py
@@ -1,7 +1,8 @@
 import json
 import threading
+from threading import Thread, Event
 from websocket import create_connection
-from zaifapi.impl import ZaifPublicApi, ZaifPrivateApi
+from zaifapi.impl import ZaifPublicApi, ZaifPrivateApi, ZaifPublicStreamApi
 from zaifbot.bot_common.config import load_config
 from zaifbot.bot_common.save_trade_log import save_trade_log
 from zaifbot.bot_common.logger import logger
@@ -9,45 +10,80 @@ from time import time
 
 
 class _ZaifWebSocket:
-    _WEB_SOCKET_API_URI = 'ws://{}:{}/stream?currency_pair={}'
     _instance = None
     _lock = threading.Lock()
-    _ws = None
-    _config = None
+    _threads = {}
+    _stop_events = {}
+    _last_prices = {}
 
     def __new__(cls):
         with cls._lock:
             if cls._instance is None:
                 cls._instance = super().__new__(cls)
-                cls._config = load_config()
-                cls._ws = cls._get_connection()
         return cls._instance
 
-    def __del__(self):
-        self._ws.close()
+    def last_price(self, currency_pair):
+        if currency_pair not in self._threads:
+            self._create_stream_thread(currency_pair)
+        target_thread = self._threads[currency_pair]
+        if target_thread.is_error_happened:
+            self._create_stream_thread(currency_pair)
+        if target_thread.is_alive() is False:
+            raise Exception('だめだめでした')
+        return target_thread.last_price
+
+    def _create_stream_thread(self, currency_pair):
+        stop_event = Event()
+        error_event = Event()
+        thread_obj = _StreamThread(currency_pair, stop_event, error_event)
+        thread_obj.start()
+        self._threads[currency_pair] = thread_obj
+        self._stop_events[currency_pair] = stop_event
+
+    def close_all_socket(self):
+        [event.set() for event in self._stop_events.values()]
+        [thread.join() for thread in self._threads.values()]
+
+
+class _StreamThread(Thread):
+
+    def __init__(self, currency_pair, stop_event, error_event):
+        super(_StreamThread, self).__init__(name='{}_wss_stream'.format(currency_pair), daemon=True)
+        self._currency_pair = currency_pair
+        self._stop_event = stop_event
+        self._last_price = None
+        self._stream_api = ZaifPublicStreamApi()
+        self._set_first_last_price()
+        self._error_event = error_event
+
+    def run(self):
+        try:
+            for receive in self._stream_api.execute(self._currency_pair):
+                self._set_last_price(receive)
+                if self._stop_event.is_set():
+                    self._stream_api.stop()
+        except:
+            self._error_event.set()
+
+    def _set_first_last_price(self):
+        receive = self._stream_api.execute(self._currency_pair).next()
+        self._set_last_price(receive)
+
+    def _set_last_price(self, receive):
+        self._last_price = {'timestamp': receive['timestamp'], 'last_price': receive['last_price']}
+
+    @property
+    def is_error_happened(self):
+        return self._error_event.is_set()
 
     @property
     def last_price(self):
-        for count in range(self._config.system.retry_count):
-            try:
-                result = self._ws.recv()
-                json_obj = json.loads(result)
-                return json_obj['last_price']['price']
-            except Exception:
-                self._ws = self._get_connection()
-        api = ZaifPublicApi()
-        return api.last_price(self._config.system.currency_pair)['last_price']
-
-    @classmethod
-    def _get_connection(cls):
-        return create_connection(cls._WEB_SOCKET_API_URI.format(cls._config.system.api_domain,
-                                                                cls._config.system.socket.port,
-                                                                cls._config.system.currency_pair))
+        return self._last_price
 
 
-def get_current_last_price():
+def get_current_last_price(currency_pair):
     api = _ZaifWebSocket()
-    return api.last_price
+    return api.last_price(currency_pair)
 
 
 class ZaifOrder:
@@ -107,3 +143,7 @@ class ZaifOrder:
                                                    count=1)
         except Exception as e:
             return e
+
+
+if __name__ == '__main__':
+    print(get_current_last_price('btc_jpy'))
