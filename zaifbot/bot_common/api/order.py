fb6869f329549720639b15fbcc1e24a947396c0a
daikishiroi <d.shiroi@techbureau.jp>
2017-04-24 13:25:07 +0900

    fix implement

--- zaifbot/bot_common/api/order.py
+++ zaifbot/bot_common/api/order.py
@@ -13,7 +13,7 @@ class CancelOrder(metaclass=ABCMeta):
     @abstractmethod
     def execute(self):
         raise NotImplementedError
-
+    
 
 class CancelByTime(CancelOrder):
     def __init__(self, key, secret, order_id, time_limit):
@@ -38,33 +38,22 @@ class CancelByPrice(CancelOrder):
         super().__init__(key, secret, order_id)
         self._price_diff = price_diff
 
-        self._type = self._private_api.active_orders()['return']['active_orders'][self._order_id]['action']
-        self._currency_pair = self._private_api.active_orders()['return']['active_orders'][self._order_id]['currency_pair']
-        self._price = self._private_api.active_orders()['return']['active_orders'][self._order_id]['price']
-
     def execute(self):
         try:
             order = self._private_api.active_orders()['return']['active_orders'][self._order_id]
+            while True:
+                last_price = get_current_last_price(order['currency_pair'])
+                if order['action'] == 'bid':
+                    if last_price - order['price'] >= self._price_diff:
+                        self._private_api.cancel_order(order_id=self._order_id)
+                        break
+                    else:
+                        time.sleep(1)
+                else:
+                    if order['price'] - last_price >= self._price_diff:
+                        self._private_api.cancel_order(order_id=self._order_id)
+                        break
+                    else:
+                        time.sleep(1)
         except Exception as e:
             logger.error(e)
-
-        while True:
-            last_price = get_current_last_price(order['currency_pair'])
-            if order['action'] == 'bid':
-                if last_price - order['price'] >= self._price_diff:
-                    try:
-                        self._private_api.cancel_order(order_id=self._order_id)
-                        break
-                    except Exception as e:
-                        logger.error(e)
-                else:
-                    time.sleep(1)
-            else:
-                if order['price'] - last_price >= self._price_diff:
-                    try:
-                        self._private_api.cancel_order(order_id=self._order_id)
-                        break
-                    except Exception as e:
-                        logger.error(e)
-                else:
-                    time.sleep(1)
