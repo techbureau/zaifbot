107e3708dcbdb276ee48a4a2c6c53ce61dd7135a
daikishiroi <d.shiroi@techbureau.jp>
2017-04-28 10:58:36 +0900

    check behavior of func

--- zaifbot/bot_common/api/wrapper.py
+++ zaifbot/bot_common/api/wrapper.py
@@ -3,20 +3,21 @@ import time
 import random
 from zaifbot.bot_common.logger import logger
 from zaifapi.impl import ZaifTradeApi, ZaifPublicApi
+from zaifapi.api_error import ZaifApiNonceError, ZaifApiError
 
-RETRY_COUNT = 5
-WAIT_SECOND = 5
+_RETRY_COUNT = 5
+_WAIT_SECOND = 5
 
 
-def with_retry(func):
+def _with_retry(func):
     def _wrapper(self, *args, **kwargs):
-        for i in range(RETRY_COUNT):
+        for i in range(_RETRY_COUNT):
             try:
                 return func(self, *args, **kwargs)
             except ZaifApiError as e:
                 logger.error(e)
                 logger.error(traceback.format_exc())
-                raise
+                raise e
             except ZaifApiNonceError as e:
                 logger.error(e)
                 logger.error(traceback.format_exc())
@@ -26,86 +27,86 @@ def with_retry(func):
             except Exception as e:
                 logger.error(e)
                 logger.error(traceback.format_exc())
-                time.sleep(WAIT_SECOND)
+                time.sleep(_WAIT_SECOND)
                 continue
     return _wrapper
 
 
 class BotTradeApi(ZaifTradeApi):
-    def __init__(self, key, secret, nonce=None):
-        super().__init__(key, secret, nonce)
+    def __init__(self, key, secret):
+        super().__init__(key, secret)
 
-    @with_retry
+    @_with_retry
     def active_orders(self, **kwargs):
         return super().active_orders(**kwargs)
 
-    @with_retry
+    @_with_retry
     def cancel_order(self, **kwargs):
         return super().cancel_order(**kwargs)
 
-    @with_retry
+    @_with_retry
     def deposit_history(self, **kwargs):
         return super().deposit_history(**kwargs)
 
-    @with_retry
+    @_with_retry
     def get_id_info(self):
         return super().get_id_info()
 
-    @with_retry
+    @_with_retry
     def get_info(self):
         return super().get_info()
 
-    @with_retry
+    @_with_retry
     def get_info2(self):
         return super().get_info2()
 
-    @with_retry
+    @_with_retry
     def get_personal_info(self):
         return super().get_personal_info()
 
-    @with_retry
+    @_with_retry
     def trade(self, **kwargs):
         return super().trade(**kwargs)
 
-    @with_retry
+    @_with_retry
     def trade_history(self, **kwargs):
         return super().trade_history(**kwargs)
 
-    @with_retry
+    @_with_retry
     def withdraw(self, **kwargs):
         return super().withdraw(**kwargs)
 
-    @with_retry
+    @_with_retry
     def withdraw_history(self, **kwargs):
         return super().withdraw_history(**kwargs)
 
 
 class BotPublicApi(ZaifPublicApi):
 
-    @with_retry
+    @_with_retry
     def last_price(self, currency_pair):
         return super().last_price(currency_pair)
 
-    @with_retry
+    @_with_retry
     def ticker(self, currency_pair):
         return super().ticker(currency_pair)
 
-    @with_retry
+    @_with_retry
     def trades(self, currency_pair):
         return super().trades(currency_pair)
 
-    @with_retry
+    @_with_retry
     def depth(self, currency_pair):
         return super().depth(currency_pair)
 
-    @with_retry
+    @_with_retry
     def currency_pairs(self, currency_pair):
         return super().currency_pairs(currency_pair)
 
-    @with_retry
+    @_with_retry
     def currencies(self, currency):
         return super().currencies(currency)
 
-    @with_retry
+    @_with_retry
     def everything(self, func_name, currency_pair, params):
         return super().everything(func_name, currency_pair, params)
