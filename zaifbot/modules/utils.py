49f0957e14bba71272d8294216a5058bb6054b23
daikishiroi <d.shiroi@techbureau.jp>
2017-05-12 12:33:23 +0900

    fix function

--- zaifbot/modules/utils.py
+++ zaifbot/modules/utils.py
@@ -7,16 +7,7 @@ def get_current_last_price(currency_pair):
     return api.last_price(currency_pair)
 
 
-def get_round_last_price(currency_pair, *, is_buy):
-    return _round_price(currency_pair, get_current_last_price(), is_buy=is_buy)
-
-
-def get_buyable_amount(currency_pair, amount, price):
-    buyable_amount = amount / price
-    return _round_amount(currency_pair, buyable_amount)
-
-
-def _round_price(currency_pair, price, *, is_buy):
+def get_round_price(currency_pair, price, *, is_buy):
     currency_pair_info = _get_currency_pair_info(currency_pair)
     if is_buy:
         return price['last_price'] + \
@@ -25,6 +16,11 @@ def _round_price(currency_pair, price, *, is_buy):
     else:
         return price['last_price'] - \
                (price['last_price'] % currency_pair_info['aux_unit_step'])
+
+
+def get_buyable_amount(currency_pair, amount, price):
+    buyable_amount = amount / price
+    return _round_amount(currency_pair, buyable_amount)
 
 
 def _round_amount(currency_pair, amount):
