3108f941abcbcf26a239b26d7677583fb8091bbe
daikishiroi <d.shiroi@techbureau.jp>
2017-05-15 11:46:23 +0900

    rename

--- zaifbot/modules/utils.py
+++ zaifbot/modules/utils.py
@@ -7,7 +7,7 @@ def get_current_last_price(currency_pair):
     return api.last_price(currency_pair)
 
 
-def get_round_price(currency_pair, price, *, is_buy):
+def get_more_executable_price(currency_pair, price, *, is_buy):
     currency_pair_info = _get_currency_pair_info(currency_pair)
     if is_buy:
         return price + (currency_pair_info['aux_unit_step'] -
