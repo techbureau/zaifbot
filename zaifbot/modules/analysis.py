d8cb7b4fcd7ee0857b687e0bc4e4a7ec3b1ca5d5
daikishiroi <d.shiroi@techbureau.jp>
2017-05-29 12:55:52 +0900

    draw with ploty

--- zaifbot/modules/analysis.py
+++ zaifbot/modules/analysis.py
@@ -3,11 +3,15 @@ import pandas as pd
 from pandas import DataFrame
 from zaifbot.modules.utils import get_price_info
 from plotly.offline import init_notebook_mode, iplot
-from plotly.tools import FigureFactory as FF
+from plotly.figure_factory import create_candlestick
 
 
-# todo: currency_pair を複数指定する
 def draw_candle_chart(currency_pair, period='1d', count=20, to_epoch_time=None):
+    fig = _get_candle_chart_fig(currency_pair, period, count, to_epoch_time)
+    iplot(fig)
+
+
+def _get_candle_chart_fig(currency_pair, period='1d', count=20, to_epoch_time=None):
     to_epoch_time = int(time.time()) if to_epoch_time is None else to_epoch_time
 
     # データの取得
@@ -15,5 +19,11 @@ def draw_candle_chart(currency_pair, period='1d', count=20, to_epoch_time=None):
     df = df[['open', 'high', 'low', 'close', 'volume', 'time']]
     df['time'] = pd.to_datetime(df['time'], unit='s')
     init_notebook_mode(connected=True)
-    fig = FF.create_candlestick(df.open, df.high, df.low, df.close, dates=df.time)
-    iplot(fig)
+    fig = create_candlestick(df.open, df.high, df.low, df.close, dates=df.time)
+    fig['layout'].update({
+        'xaxis': {
+            'showgrid': True
+        }
+    })
+    return fig
+
