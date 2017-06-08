12039375f2b2ed122bee59375ab9e326bb3e7505
RossyWhite <daikishiroi@gmail.com>
2017-06-08 15:46:05 +0900

    rename trade_logs to ohlc_prices

--- zaifbot/moving_average.py
+++ zaifbot/moving_average.py
@@ -1,7 +1,7 @@
 import time
 from zaifbot.bot_common.bot_const import PERIOD_SECS, LIMIT_COUNT, LIMIT_LENGTH, UTC_JP_DIFF
-from zaifbot.modules.moving_average import TradeLogsSetUp, MovingAverageSetUp
-from zaifbot.modules.dao.moving_average import MovingAverageDao, TradeLogsDao
+from zaifbot.modules.moving_average import OhlcPricesSetUp, MovingAverageSetUp
+from zaifbot.modules.dao.moving_average import MovingAverageDao, OhlcPricesDao
 import pandas as pd
 
 
@@ -26,9 +26,9 @@ def _get_moving_average(currency_pair, period, count, to_epoch_time, length, sma
     tl_start_time = end_time - ((count + length) * PERIOD_SECS[period])
     ma_start_time = end_time - (count * PERIOD_SECS[period])
 
-    trade_logs = TradeLogsSetUp(currency_pair, period, count, length)
-    if trade_logs.execute(tl_start_time, end_time) is False:
-        return {'success': 0, 'error': 'failed to set up trade log'}
+    ohlc_prices = OhlcPricesSetUp(currency_pair, period, count, length)
+    if ohlc_prices.execute(tl_start_time, end_time) is False:
+        return {'success': 0, 'error': 'failed to set up ohlc data'}
 
     moving_average = MovingAverageSetUp(currency_pair, period, count, length)
     ma_result = moving_average.execute(ma_start_time, tl_start_time, end_time)
@@ -49,17 +49,17 @@ def get_end_time(to_epoch_time, period):
 def _create_return_dict(sma_ema, currency_pair, period, length, end_time, tl_start_time, count):
     return_datas = []
     moving_average_dao = MovingAverageDao(currency_pair, period, length)
-    trade_logs_dao = TradeLogsDao(currency_pair, period)
+    ohlc_prices_dao = OhlcPricesDao(currency_pair, period)
     moving_average = \
         [i.__dict__ for i in moving_average_dao.get_records(end_time, tl_start_time, False)]
-    trade_logs = \
-        [i.__dict__ for i in trade_logs_dao.get_records(end_time, tl_start_time, False)]
+    ohlc_prices = \
+        [i.__dict__ for i in ohlc_prices_dao.get_records(end_time, tl_start_time, False)]
     moving_average_df = \
         pd.DataFrame(moving_average, columns=['time', sma_ema])
-    trade_logs_df = \
-        pd.DataFrame(trade_logs, columns=['time', 'close', 'closed'])
+    ohlc_prices_df = \
+        pd.DataFrame(ohlc_prices, columns=['time', 'close', 'closed'])
     ma_result = \
-        trade_logs_df.merge(moving_average_df, left_on='time', right_on='time', how='inner')
+        ohlc_prices_df.merge(moving_average_df, left_on='time', right_on='time', how='inner')
     ma_result.rename(columns={'time': 'time_stamp', sma_ema: 'moving_average'}, inplace=True)
     ma_result_length = len(ma_result.index)
     if ma_result_length == 0:
