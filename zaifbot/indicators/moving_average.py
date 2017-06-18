c996b968b488db3b8ccb699e6b5faff7d7c1ba7e
RossyWhite <daikishiroi@gmail.com>
2017-06-18 18:31:49 +0900

    delete get_moving_average

--- zaifbot/indicators/moving_average.py
+++ zaifbot/indicators/moving_average.py
@@ -18,7 +18,12 @@ class MA(Indicator):
     def get_data(self):
         raise NotImplementedError
 
-    def _get_moving_average(self, count, to_epoch_time, sma_ema):
+
+class EMA(MA):
+    def __init__(self, currency_pair='btc_jpy', period='1d', length=LIMIT_LENGTH):
+        super().__init__(currency_pair, period, length)
+
+    def get_data(self, count=LIMIT_COUNT, to_epoch_time=None):
         to_epoch_time = int(time.time()) if to_epoch_time is None else to_epoch_time
         count = min(count, LIMIT_COUNT)
         length = min(self._length, LIMIT_LENGTH)
@@ -29,29 +34,12 @@ class MA(Indicator):
 
         if len(ohlc_prices_result.index) == 0:
             return {'success': 0, 'error': 'failed to get ohlc price'}
-        sma = ab.SMA(ohlc_prices_result, timeperiod=length)
+
         ema = ab.EMA(ohlc_prices_result, timeperiod=length)
         ohlc_prices_result = \
-            ohlc_prices_result.merge(sma.to_frame(), left_index=True, right_index=True) \
-                .rename(columns={0: 'sma'})
-        ohlc_prices_result = \
             ohlc_prices_result.merge(ema.to_frame(), left_index=True, right_index=True) \
-                .rename(columns={0: 'ema'})
-        if sma_ema == 'sma':
-            ohlc_prices_result = ohlc_prices_result[-count:].drop('ema', axis=1) \
-                .rename(columns={sma_ema: 'moving_average'}).to_dict(orient='records')
-        else:
-            ohlc_prices_result = ohlc_prices_result[-count:].drop('ema', axis=1) \
-                .rename(columns={sma_ema: 'moving_average'}).to_dict(orient='records')
-        return {'success': 1, 'return': {sma_ema: ohlc_prices_result}}
-
-
-class EMA(MA):
-    def __init__(self, currency_pair='btc_jpy', period='1d', length=LIMIT_LENGTH):
-        super().__init__(currency_pair, period, length)
-
-    def get_data(self, count=LIMIT_COUNT, to_epoch_time=None):
-        return self._get_moving_average(count, to_epoch_time, 'ema')
+                .rename(columns={0: 'ema'}).to_dict(orient='records')
+        return {'success': 1, 'return': {'ema': ohlc_prices_result}}
 
 
 class SMA(MA):
@@ -59,4 +47,20 @@ class SMA(MA):
         super().__init__(currency_pair, period, length)
 
     def get_data(self, count=LIMIT_COUNT, to_epoch_time=None):
-        return self._get_moving_average(count, to_epoch_time, 'sma')
+        to_epoch_time = int(time.time()) if to_epoch_time is None else to_epoch_time
+        count = min(count, LIMIT_COUNT)
+        length = min(self._length, LIMIT_LENGTH)
+        end_time = truncate_time_at_period(to_epoch_time, self._period)
+        tl_start_time = end_time - ((count + length) * PERIOD_SECS[self._period])
+        ohlc_prices = OhlcPrices(self._currency_pair, self._period, count, length)
+        ohlc_prices_result = ohlc_prices.execute(tl_start_time, end_time)
+
+        if len(ohlc_prices_result.index) == 0:
+            return {'success': 0, 'error': 'failed to get ohlc price'}
+
+        sma = ab.SMA(ohlc_prices_result, timeperiod=length)
+
+        ohlc_prices_result = \
+            ohlc_prices_result.merge(sma.to_frame(), left_index=True, right_index=True) \
+                .rename(columns={0: 'sma'}).to_dict(orient='records')
+        return {'success': 1, 'return': {'sma': ohlc_prices_result}}
