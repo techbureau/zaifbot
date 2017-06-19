efd150d3549fb2806764ff15c70bc21f5c7f5da7
RossyWhite <daikishiroi@gmail.com>
2017-06-19 15:07:51 +0900

    independent ohlcprices

--- zaifbot/price/ohlc_prices.py
+++ zaifbot/price/ohlc_prices.py
@@ -15,7 +15,6 @@ def get_price_info(currency_pair, period='1d', count=5, to_epoch_time=None):
     return public_api.everything('ohlc_data', currency_pair, second_api_params)
 
 
-# todo: 処理に無駄が多い。
 class OhlcPrices:
     def __init__(self, currency_pair, period):
         self._currency_pair = currency_pair
@@ -38,111 +37,16 @@ class OhlcPrices:
         public_api = BotPublicApi()
         api_params = {'period': self._period, 'count': count, 'to_epoch_time': to_epoch_time}
         records = public_api.everything('ohlc_data', self._currency_pair, api_params)
-        dao.create_data(records)
-        # dbに保存するロジックを書く
+        self._dao.create_data(records)
         return records
 
     def _fetch_data_from_db(self, start_time, end_time):
-        records = self._dao.get_records(start_time=start_time, end_time=end_time, closed=True)
+        records = list(map(self._row2dict, self._dao.get_records(end_time, start_time, closed=False)))
         return records
 
-if __name__ == '__main__':
-    price = OhlcPrices(currency_pair='btc_jpy', period='1d')
-    a = price.fetch_data(count=1)
-    print(a)
-
-
-
-    # def execute(self, start_time, end_time, count, length):
-    #
-    #
-    #     api_records = pd.DataFrame(self._get_ohlc_data_from_server(end_time))
-    #
-    #     if api_records.empty:
-    #         return False
-    #     target_ohlc_prices_record = api_records.join(target_epoch_times, on='time', how='inner')
-    #     target_ohlc_prices_record['currency_pair'] = self._currency_pair
-    #     target_ohlc_prices_record['period'] = self._period
-    #     self._dao.create_data(target_ohlc_prices_record)
-    #     return api_records[-(count + length):][['close', 'closed', 'time']]
-    #
-    #
-    #
-    # def _get_ohlc_data_from_server(self, end_time, count, length):
-    #     public_api = BotPublicApi()
-    #     api_params = {'period': self._period, 'count': LIMIT_COUNT, 'to_epoch_time': end_time + 1}
-    #     try:
-    #         api_record = public_api.everything('ohlc_data', self._currency_pair, api_params)
-    #     except Exception as e:
-    #         logger.error(e, exc_info=True)
-    #         api_record = []
-    #     required_count = count + length
-    #     if required_count <= LIMIT_COUNT:
-    #         return api_record
-    #     count = required_count - LIMIT_COUNT
-    #     second_end_time = end_time - (LIMIT_COUNT * PERIOD_SECS[self._period]) + 1
-    #     second_api_params =\
-    #         {'period': self._period, 'count': count, 'to_epoch_time': second_end_time}
-    #     try:
-    #         second_api_record =\
-    #             public_api.everything('ohlc_data', self._currency_pair, second_api_params)
-    #     except Exception as e:
-    #         logger.error(e, exc_info=True)
-    #         second_api_record = []
-    #     api_record = second_api_record + api_record
-    #     return api_record
-    #
-    #
-    #
-
-
-# class OhlcPricesold:
-#     def __init__(self, currency_pair, period, count, length):
-#         self._currency_pair = currency_pair
-#         self._period = period
-#         self._count = count
-#         self._length = length
-#         self._ohlc_prices = OhlcPricesDao(self._currency_pair, self._period)
-#
-#     def execute(self, start_time, end_time):
-#         target_epoch_times = pd.DataFrame(index=self._get_target_epoch_times(start_time, end_time))
-#         if len(target_epoch_times.index) == 0:
-#             return True
-#         api_records = pd.DataFrame(self._get_ohlc_data_from_server(end_time))
-#
-#         if api_records.empty:
-#             return False
-#         target_ohlc_prices_record = api_records.join(target_epoch_times, on='time', how='inner')
-#         target_ohlc_prices_record['currency_pair'] = self._currency_pair
-#         target_ohlc_prices_record['period'] = self._period
-#         self._ohlc_prices.create_data(target_ohlc_prices_record)
-#         return api_records[-(self._count + self._length):][['close', 'closed', 'time']]
-#
-#     def _get_target_epoch_times(self, start_time, end_time):
-#         ohlc_prices_record = \
-#             set(x.time for x in self._ohlc_prices.get_records(end_time, start_time, True))
-#         return check_missing_records(ohlc_prices_record, start_time, end_time, self._period)
-#
-#     def _get_ohlc_data_from_server(self, end_time):
-#         public_api = BotPublicApi()
-#         api_params = {'period': self._period, 'count': LIMIT_COUNT, 'to_epoch_time': end_time + 1}
-#         try:
-#             api_record = public_api.everything('ohlc_data', self._currency_pair, api_params)
-#         except Exception as e:
-#             logger.error(e, exc_info=True)
-#             api_record = []
-#         required_count = self._count + self._length
-#         if required_count <= LIMIT_COUNT:
-#             return api_record
-#         count = required_count - LIMIT_COUNT
-#         second_end_time = end_time - (LIMIT_COUNT * PERIOD_SECS[self._period]) + 1
-#         second_api_params =\
-#             {'period': self._period, 'count': count, 'to_epoch_time': second_end_time}
-#         try:
-#             second_api_record =\
-#                 public_api.everything('ohlc_data', self._currency_pair, second_api_params)
-#         except Exception as e:
-#             logger.error(e, exc_info=True)
-#             second_api_record = []
-#         api_record = second_api_record + api_record
-#         return api_record
+    # todo: もっと汎用的な場所に移動させる
+    @staticmethod
+    def _row2dict(row):
+        dict_row = row.__dict__
+        dict_row.pop('_sa_instance_state', None)
+        return dict_row
