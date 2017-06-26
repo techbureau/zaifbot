04a5450b25b71d26bf82042589eada8da5f9ad7a
RossyWhite <daikishiroi@gmail.com>
2017-06-26 17:47:48 +0900

    edit directory

--- zaifbot/api/__init__.py
+++ zaifbot/api/__init__.py
@@ -1,9 +1,6 @@
 __all__ = []
 
 from .order import *
-from .wrapper import *
-
-__all__ += wrapper.__all__
 __all__ += order.__all__
 
 
--- zaifbot/order/auto_cancel.py
+++ zaifbot/order/auto_cancel.py
@@ -1,27 +1,10 @@
 import time
 from abc import ABCMeta, abstractmethod
-from threading import Thread, Event, Lock
-from uuid import uuid4
+from threading import Thread, Event
 from zaifbot.bot_common.errors import ZaifBotError
 from zaifbot.price.cache import ZaifCurrencyPairs
 from zaifbot.price.utils import get_current_last_price
-
-
-class BotOrderID:
-    order_ids = set()
-
-    def __init__(self):
-        self._id = self._gen_id()
-
-    @classmethod
-    def _gen_id(cls):
-        order_id = str(uuid4())
-        if order_id in cls.order_ids:
-            cls._gen_id()
-        return order_id
-
-    def __str__(self):
-        return self._id
+from zaifbot.order.common import ActiveOrders, BotOrderID
 
 
 class AutoCancel:
@@ -155,47 +138,3 @@ class _AutoCancelByPrice(_AutoCancelOrder):
     @property
     def name(self):
         return 'PriceRangeCancel'
-
-
-class ActiveOrders:
-    _instance = None
-    _lock = Lock()
-    _thread = Thread()
-    _active_orders = {}
-    _api = None
-
-    def __new__(cls, api, *args, **kwargs):
-        with cls._lock:
-            if cls._instance is None:
-                cls._instance = super().__new__(cls)
-                cls._api = api
-        return cls._instance
-
-    def find(self, bot_order_id):
-        return self._active_orders.get(bot_order_id, None)
-
-    def add(self, order):
-        bot_order_id = order.info['bot_order_id']
-        self._active_orders[bot_order_id] = order
-
-    def remove(self, bot_order_id):
-        self._active_orders.pop(bot_order_id)
-
-    def all(self):
-        return [order.info for order in self._active_orders.values()]
-
-    def update(self):
-        with self._lock:
-            remote_order_ids = self._fetch_remote_order_ids()
-            for active_order in self._active_orders:
-                if active_order.info.get('zaif_order_id') not in remote_order_ids:
-                    self._active_orders.pop(active_order.info.bot_order_id)
-            for active_order in self._active_orders:
-                if active_order.is_alive is False:
-                    self._active_orders.pop(active_order.info.bot_order_id)
-
-    @classmethod
-    def _fetch_remote_order_ids(cls):
-        orders = cls._api.active_orders(is_token_both=True)
-        return orders['token_active_orders'].keys() + orders['active_orders'].keys()
-
