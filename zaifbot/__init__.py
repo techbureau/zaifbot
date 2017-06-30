1b1a00cd8a3bc75fa10e474faa86cc22959c5a4f
RossyWhite <daikishiroi@gmail.com>
2017-06-30 16:58:04 +0900

    edit logic

--- zaifbot/__init__.py
+++ zaifbot/__init__.py
@@ -18,30 +18,28 @@ class ZaifBot:
         [x.join() for x in running_processes]
 
 
-# Todo:　ひどすぎる
 def install_ta_lib():
     parent_path = os.path.dirname(os.path.abspath(__file__))
     os.chdir(parent_path + "/setup")
     if sys.platform.startswith('linux'):
         subprocess.call(["./install_ta_lib.sh"])
     elif sys.platform.startswith('mac'):
-        # todo: 実装
-        pass
+        subprocess.call(["brew", "install", "ta-lib"])
     elif sys.platform.startswith('win'):
         bits = '32' if sys.maxsize < 2 ** 31 else '64'
-        pyv = str(sys.version_info.major) + str(sys.version_info.minor)
-        if bits == 32:
-            file = os.path.join(os.path.dirname(__file__),
-                                "setup/TA_Lib-0.4.10-cp{v}-cp{v}m-win32.whl".format(v=pyv))
-            if os.path.isfile(file):
-                subprocess.call(["pip", "install", file])
-            else:
-                raise ZaifBotError('this library does not  support your platform')
+        python_v = str(sys.version_info.major) + str(sys.version_info.minor)
+        __install_talib_for_windows(bits, python_v)
 
-        else:
-            file = os.path.join(os.path.dirname(__file__),
-                                "setup/TA_Lib-0.4.10-cp{v}-cp{v}m-win_amd64.whl".format(v=pyv))
-            if os.path.isfile(file):
-                subprocess.call(["pip", "install", file])
-            else:
-                raise ZaifBotError('this library does not  support your platform')
+
+def __install_talib_for_windows(bits, python_v):
+    if bits == 32:
+        file = os.path.join(os.path.dirname(__file__),
+                            "setup/TA_Lib-0.4.10-cp{v}-cp{v}m-win32.whl".format(v=python_v))
+    else:
+        file = os.path.join(os.path.dirname(__file__),
+                            "setup/TA_Lib-0.4.10-cp{v}-cp{v}m-win_amd64.whl".format(v=python_v))
+
+    if os.path.isfile(file):
+        subprocess.call(["pip", "install", file])
+    else:
+        raise ZaifBotError('this library does not  support your platform')
