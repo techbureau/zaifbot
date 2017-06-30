d07e0b1d5df8bea58c89a42f06076524ae85abce
RossyWhite <daikishiroi@gmail.com>
2017-06-30 22:04:14 +0900

    install working on linux, windows, OSX

--- zaifbot/__init__.py
+++ zaifbot/__init__.py
@@ -19,27 +19,34 @@ class ZaifBot:
 
 
 def install_ta_lib():
-    parent_path = os.path.dirname(os.path.abspath(__file__))
-    os.chdir(parent_path + "/setup")
     if sys.platform.startswith('linux'):
-        subprocess.call(["./install_ta_lib.sh"])
-    elif sys.platform.startswith('mac'):
+        cwd = os.path.join(os.path.dirname(__file__), './setup')
+        subprocess.call(["./install_ta_lib.sh"], cwd=cwd)
+        return
+
+    if sys.platform.startswith('darwin'):
         subprocess.call(["brew", "install", "ta-lib"])
-    elif sys.platform.startswith('win'):
+        return
+
+    if sys.platform.startswith('win'):
         bits = '32' if sys.maxsize < 2 ** 31 else '64'
-        python_v = str(sys.version_info.major) + str(sys.version_info.minor)
-        __install_talib_for_windows(bits, python_v)
+        py_version = str(sys.version_info.major) + str(sys.version_info.minor)
+        __install_talib_for_windows(bits, py_version)
+        return
+
+    raise ZaifBotError('zaifbot does not support your platform')
 
 
-def __install_talib_for_windows(bits, python_v):
+def __install_talib_for_windows(bits, py_version):
     if bits == 32:
         file = os.path.join(os.path.dirname(__file__),
-                            "setup/TA_Lib-0.4.10-cp{v}-cp{v}m-win32.whl".format(v=python_v))
+                            "setup/TA_Lib-0.4.10-cp{v}-cp{v}m-win32.whl".format(v=py_version))
     else:
         file = os.path.join(os.path.dirname(__file__),
-                            "setup/TA_Lib-0.4.10-cp{v}-cp{v}m-win_amd64.whl".format(v=python_v))
+                            "setup/TA_Lib-0.4.10-cp{v}-cp{v}m-win_amd64.whl".format(v=py_version))
 
     if os.path.isfile(file):
         subprocess.call(["pip", "install", file])
-    else:
-        raise ZaifBotError('this library does not  support your platform')
+        return
+
+    raise ZaifBotError('zaifbot does not support your platform')
