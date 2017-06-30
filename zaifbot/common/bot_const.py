d0fc18add0dc6d750d4d74ae78a76835b2436518
RossyWhite <daikishiroi@gmail.com>
2017-06-30 18:42:27 +0900

    fix small things

--- zaifbot/common/bot_const.py
+++ zaifbot/common/bot_const.py
@@ -5,23 +5,9 @@ class Action(Enum):
     Buy = 'bid'
     Sell = 'ask'
 
-
+# todo: 定数使わないようにする。
 PERIOD_SECS = {'1d': 86400, '12h': 43200, '8h': 28800, '4h': 14400,
                '1h': 3600, '1m': 60, '5m': 300, '15m': 900, '30m': 1800}
 LIMIT_COUNT = 1000
 LIMIT_LENGTH = 100
 UTC_JP_DIFF = 32400
-CLOSED = 1
-CANCEL = 2
-STOP_LOSS = 2
-
-
-class Action(Enum):
-    Buy = 'bid'
-    Sell = 'ask'
-
-
-if __name__ == '__main__':
-    action = Action('bid')
-    print(action)
-    print(action is Action.Buy)
