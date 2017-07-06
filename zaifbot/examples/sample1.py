5bfd396292fcdffb9da6ff77a0cadca0a8b15093
RossyWhite <daikishiroi@gmail.com>
2017-07-06 17:48:14 +0900

    buylower and sell heiger

---
+++ zaifbot/examples/sample1.py
@@ -0,0 +1,5 @@
+from zaifbot.strategy import Strategy
+from zaifbot.rules.entry.buy_lower import MyEntry
+
+
+my_strategy = Strategy(1,3, 4,)
---
+++ zaifbot/rules/entry/buy_lower.py
@@ -0,0 +1,5 @@
+from zaifbot.rules.entry import Entry
+
+class MyEntry(Entry):
+    def can_entry(self):
+        pass
