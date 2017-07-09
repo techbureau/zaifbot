ab49b8519a9e2e70634dbb3ce161720943cf0ee9
RossyWhite <daikishiroi@gmail.com>
2017-07-09 19:35:45 +0900

    replace truncate time

--- zaifbot/ohlc_prices.py
+++ zaifbot/ohlc_prices.py
@@ -2,7 +2,8 @@ import time
 
 from .web import BotPublicApi
 from zaifbot.dao.dao import OhlcPricesDao
-from zaifbot.utils import calc_start_from_count_and_end, truncate_time_at_period, merge_dict
+from zaifbot.utils import merge_dict
+from zaifbot.common.period import Period
 
 
 class OhlcPrices:
@@ -10,14 +11,14 @@ class OhlcPrices:
 
     def __init__(self, currency_pair, period):
         self._currency_pair = currency_pair
-        self._period = period
+        self._period = Period(period)
         self._dao = OhlcPricesDao(self._currency_pair, self._period)
 
     def fetch_data(self, count=100, to_epoch_time=None):
         count = min(count, self.MAX_COUNT)
         to_epoch_time = to_epoch_time or int(time.time())
-        end_time_rounded = truncate_time_at_period(to_epoch_time, self._period)
-        start_time = calc_start_from_count_and_end(count, end_time_rounded, self._period)
+        end_time_rounded = self._period.truncate_sec(to_epoch_time)
+        start_time = self._period.calc_start(count, end_time_rounded)
 
         db_records = self._fetch_data_from_db(start_time=start_time, end_time=end_time_rounded)
         if len(db_records) >= count:
