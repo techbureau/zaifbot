e5c9d4f471583f32d697d11aa884d0c98e41f280
RossyWhite <daikishiroi@gmail.com>
2017-07-21 11:16:33 +0900

    Clean inside ma

--- zaifbot/indicators/indicator.py
+++ zaifbot/indicators/indicator.py
@@ -5,18 +5,24 @@ from zaifbot.exchange.candle_sticks import CandleSticks
 
 
 class Indicator(metaclass=ABCMeta):
-    MAX_LENGTH = 100
-    MAX_COUNT = 1000
+    _MAX_LENGTH = 100
+    _MAX_COUNT = 1000
+    _NAME = None
 
     @abstractmethod
     def request_data(self, *args, **kwargs):
         raise NotImplementedError
 
     @staticmethod
-    def execute_function(name, *args, **kwargs):
+    def _execute_talib(name, *args, **kwargs):
         return abstract.Function(name)(*args, **kwargs)
 
     @staticmethod
-    def get_candlesticks_df(currency_pair, period, count, to_epoch_time):
+    def _get_candlesticks_df(currency_pair, period, count, to_epoch_time):
         candle_sticks_data = CandleSticks(currency_pair, period).request_data(count, to_epoch_time)
         return DataFrame(candle_sticks_data)
+
+    @property
+    @abstractmethod
+    def name(self):
+        raise NotImplementedError
--- zaifbot/indicators/moving_average.py
+++ zaifbot/indicators/moving_average.py
@@ -1,32 +1,21 @@
+from abc import ABCMeta
 import pandas as pd
 from zaifbot.utils import int_time
 from .indicator import Indicator
 
 
-class MA(Indicator):
+class _MA(Indicator, metaclass=ABCMeta):
     def __init__(self, currency_pair='btc_jpy', period='1d', length=25):
         self._currency_pair = currency_pair
         self._period = period
-        self._length = min(length, self.MAX_LENGTH)
+        self._length = min(length, self._MAX_LENGTH)
 
-    def request_data(self, count, to_epoch_time):
-        raise NotImplementedError
-
-    def _get_adjusted_count(self, count):
-        min(count, self.MAX_COUNT)
+    def _adjust_count(self, count):
+        min(count, self._MAX_COUNT)
         return count + self._length - 1
 
-    def _get_ma(self, count, to_epoch_time, name):
-        count, to_epoch_time = self._pre_processing(count, to_epoch_time)
-
-        candlesticks_df = self.get_candlesticks_df(self._currency_pair, self._period, count, to_epoch_time)
-        ma = self.execute_function(name, candlesticks_df, timeperiod=self._length).rename(name).dropna()
-
-        formatted_ma = self._formatting(candlesticks_df['time'], ma)
-        return formatted_ma
-
     def _pre_processing(self, count, to_epoch_time):
-        count = self._get_adjusted_count(count)
+        count = self._adjust_count(count)
         to_epoch_time = to_epoch_time or int_time()
         return count, to_epoch_time
 
@@ -35,12 +24,25 @@ class MA(Indicator):
         return pd.concat([time_df, ma], axis=1).dropna().astype(object).to_dict(orient='records')
 
 
-class EMA(MA):
+class EMA(_MA):
+    _NAME = 'ema'
+
     def __init__(self, currency_pair='btc_jpy', period='1d', length=25):
         super().__init__(currency_pair, period, length)
 
+    @property
+    def name(self):
+        return self._NAME
+
     def request_data(self, count=100, to_epoch_time=None):
-        return self._get_ma(count, to_epoch_time, 'ema')
+        adjusted_count, adjusted_to_epoch_time = self._pre_processing(count, to_epoch_time)
+        candlesticks_df = self._get_candlesticks_df(self._currency_pair,
+                                                    self._period, adjusted_count,
+                                                    adjusted_to_epoch_time)
+
+        ema = self._execute_talib(self.name, candlesticks_df, timeperiod=self._length).rename(self.name).dropna()
+        formatted_ema = self._formatting(candlesticks_df['time'], ema)
+        return formatted_ema
 
     def is_increasing(self):
         previous, last = self.request_data(2, int_time())
@@ -50,12 +52,25 @@ class EMA(MA):
         return not self.is_increasing()
 
 
-class SMA(MA):
+class SMA(_MA):
+    _NAME = 'sma'
+
     def __init__(self, currency_pair='btc_jpy', period='1d', length=25):
         super().__init__(currency_pair, period, length)
 
+    @property
+    def name(self):
+        return self._NAME
+
     def request_data(self, count=100, to_epoch_time=None):
-        return self._get_ma(count, to_epoch_time, 'sma')
+        adjusted_count, adjusted_to_epoch_time = self._pre_processing(count, to_epoch_time)
+        candlesticks_df = self._get_candlesticks_df(self._currency_pair,
+                                                    self._period, adjusted_count,
+                                                    adjusted_to_epoch_time)
+
+        sma = self._execute_talib(self.name, candlesticks_df, timeperiod=self._length).rename(self.name).dropna()
+        formatted_sma = self._formatting(candlesticks_df['time'], sma)
+        return formatted_sma
 
     def is_increasing(self):
         previous, last = self.request_data(2, int_time())
