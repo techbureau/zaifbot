0a4bd6562d7786bc7e222bfcaa39bffba1381c56
RossyWhite <daikishiroi@gmail.com>
2017-07-21 10:40:01 +0900

    Add get_candlesticks_df

--- zaifbot/indicators/indicator.py
+++ zaifbot/indicators/indicator.py
@@ -1,5 +1,7 @@
 from abc import ABCMeta, abstractmethod
 from talib import abstract
+from pandas import DataFrame
+from zaifbot.exchange.candle_sticks import CandleSticks
 
 
 class Indicator(metaclass=ABCMeta):
@@ -13,3 +15,9 @@ class Indicator(metaclass=ABCMeta):
     @staticmethod
     def execute_function(name, *args, **kwargs):
         return abstract.Function(name)(*args, **kwargs)
+
+    @staticmethod
+    def get_candlesticks_df(currency_pair, period, count, to_epoch_time):
+        candle_sticks_data = CandleSticks(currency_pair, period).request_data(count, to_epoch_time)
+        return DataFrame(candle_sticks_data)
+
