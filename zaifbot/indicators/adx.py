c7d066ad85348168165b051d81896ac5b7543185
RossyWhite <daikishiroi@gmail.com>
2017-07-24 12:24:32 +0900

    Fix adx

--- zaifbot/indicators/adx.py
+++ zaifbot/indicators/adx.py
@@ -1,11 +1,5 @@
 import pandas as pd
-
 from .indicator import Indicator
-
-_HIGH = 'high'
-_LOW = 'low'
-_CLOSE = 'close'
-_TIME = 'time'
 
 
 class ADX(Indicator):
@@ -18,14 +12,19 @@ class ADX(Indicator):
     def request_data(self, count=100, to_epoch_time=None):
         candlesticks_df = self._get_candlesticks_df(count, to_epoch_time)
 
-        adx = self._exec_talib_func(candlesticks_df, timeperiod=self._length, prices=[_HIGH, _LOW, _CLOSE], output_names=['adx']).rename('adx')
+        adx = self._exec_talib_func(candlesticks_df,
+                                    timeperiod=self._length,
+                                    prices=['high', 'low', 'close'])
 
-        formatted_adx = self._formatting(candlesticks_df[_TIME], adx)
+        formatted_adx = self._formatting(candlesticks_df, adx)
         return formatted_adx
 
     def _required_candlesticks_count(self, count):
         return 2 * self._length - 1 + self._bounded_count(count)
 
-    def _formatting(self, time_df, adx):
-        adx = pd.concat([time_df, adx], axis=1).dropna()
-        return adx.astype(object).to_dict(orient='records')
+    def _formatting(self, candlesticks_df, adx):
+        adx.rename(self.name, inplace=True)
+        adx_with_time = pd.concat([candlesticks_df['time'], adx], axis=1)
+        adx_with_time.dropna(inplace=True)
+        dict_adx = adx_with_time.astype(object).to_dict(orient='records')
+        return dict_adx
