ad7dd56d08f5a007790946580f82a317035025bd
RossyWhite <daikishiroi@gmail.com>
2017-07-24 11:22:01 +0900

    Edit macd data format

--- zaifbot/indicators/macd.py
+++ zaifbot/indicators/macd.py
@@ -1,12 +1,5 @@
 import pandas as pd
-
-
 from .indicator import Indicator
-
-_HIGH = 'high'
-_LOW = 'low'
-_CLOSE = 'close'
-_TIME = 'time'
 
 
 class MACD(Indicator):
@@ -20,15 +13,17 @@ class MACD(Indicator):
 
     def request_data(self, count=100, to_epoch_time=None):
         candlesticks_df = self._get_candlesticks_df(count, to_epoch_time)
-        macd = self._exec_talib_func(candlesticks_df, price=_CLOSE, fastperiod=self._short, slowperiod=self._long, signalperiod=self._signal)
+        macd = self._exec_talib_func(candlesticks_df, price='close', fastperiod=self._short, slowperiod=self._long, signalperiod=self._signal)
 
-        formatted_macd = self._formatting(candlesticks_df[_TIME], macd)
+        formatted_macd = self._formatting(candlesticks_df, macd)
         return formatted_macd
 
     def _required_candlesticks_count(self, count):
         return self._bounded_count(count) + self._long + self._signal - 2
 
     @staticmethod
-    def _formatting(time_df, macd):
-        macd = pd.concat([time_df, macd], axis=1).dropna()
-        return macd.astype(object).to_dict(orient='records')
+    def _formatting(candlesticks_df, macd):
+        macd_with_time = pd.concat([candlesticks_df['time'], macd], axis=1)
+        macd_with_time.dropna(inplace=True)
+        dict_macd = macd_with_time.astype(object).to_dict(orient='records')
+        return dict_macd
