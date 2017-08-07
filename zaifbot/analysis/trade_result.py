6ec71c47ec23c7014194763a73e5c8d4b728bd5c
RossyWhite <daikishiroi@gmail.com>
2017-08-07 18:01:36 +0900

    Interface of trade analysis

--- zaifbot/analysis/trade_result.py
+++ zaifbot/analysis/trade_result.py
@@ -5,21 +5,57 @@ class TradeResult:
     def __init__(self):
         self._dao = TradesDao()
 
-    def history(self, from_datetime, to_datetime, filters=None):
+    def history(self, from_datetime=None, to_datetime=None, filters=None):
         records = self._dao.history(from_datetime=from_datetime,
                                     to_datetime=to_datetime,
                                     filters=filters)
 
         return self._dao.rows2dicts(records)
 
-    def most_profitable_trade(self):
+    def gross_profit(self, from_datetime=None, to_datetime=None, filters=None):
         pass
 
-    def least_profitable_trade(self):
+    def gross_loss(self, from_datetime=None, to_datetime=None, filters=None):
         pass
 
-    def profit_for_a_period(self, from_datetime, to_datetime, filters=None):
+    def profit(self, from_datetime=None, to_datetime=None, filters=None):
         pass
 
-    def profit_of_a_strategy(self, name, params):
+    def profit_factor(self, from_datetime=None, to_datetime=None, filters=None):
+        pass
+
+    def max_draw_down(self, from_datetime=None, to_datetime=None, filters=None):
+        pass
+
+    def trades_count(self, from_datetime=None, to_datetime=None, filters=None):
+        pass
+
+    def long_trades_count(self):
+        pass
+
+    def short_trades_count(self):
+        pass
+
+    def win_trades_count(self, from_datetime=None, to_datetime=None, filters=None):
+        pass
+
+    def lose_trades_count(self, from_datetime=None, to_datetime=None, filters=None):
+        pass
+
+    def win_trades_percent(self, from_datetime=None, to_datetime=None, filters=None):
+        pass
+
+    def largest_win_trade(self, from_datetime=None, to_datetime=None, filters=None):
+        pass
+
+    def average_win_trade(self, from_datetime=None, to_datetime=None, filters=None):
+        pass
+
+    def largest_lose_trade(self, from_datetime=None, to_datetime=None, filters=None):
+        pass
+
+    def average_lose_trade(self, from_datetime=None, to_datetime=None, filters=None):
+        pass
+
+    def average_profit(self, from_datetime=None, to_datetime=None, filters=None):
         pass
