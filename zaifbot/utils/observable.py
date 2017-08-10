f13da4a28f34eaa329849476585c8e198c70b963
RossyWhite <daikishiroi@gmail.com>
2017-08-10 11:20:35 +0900

    Edit Observer

--- zaifbot/utils/observable.py
+++ zaifbot/utils/observable.py
@@ -1,31 +1,18 @@
 import itertools
 
 
-class Observed:
+class Observable:
     def __init__(self):
         self.__observers = set()
 
-    def obsrevers_add(self, observer, *observers):
+    def register_observers(self, observer, *observers):
         for observer in itertools.chain((observer, ), observers):
             self.__observers.add(observer)
             observer.update()
 
-    def observer_discard(self, observer):
+    def remove_observers(self, observer):
         self.__observers.discard(observer)
 
     def observers_notify(self):
         for observer in self.__observers:
             observer.update(self)
-
-
-class SliderModel(Observed):
-    def __init__(self, minimum, value, maximum):
-        super().__init__()
-        self.__minimum = self.__value = self.__maximum = None
-        self.minimum = minimum
-        self.value = value
-        self.maximum = maximum
-
-    @property
-    def value(self):
-        return self.__value
