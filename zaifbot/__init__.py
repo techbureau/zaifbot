cbae6e3ab9016bfb50e9f23a522e2eb5a9038656
RossyWhite <daikishiroi@gmail.com>
2017-08-17 15:32:06 +0900

    log customize

--- zaifbot/__init__.py
+++ zaifbot/__init__.py
@@ -12,18 +12,34 @@ def zaifbot(import_name):
     app.url_map.strict_slashes = False
     app.config['JSON_SORT_KEYS'] = False
 
+    _customize_flask_logger()
+
     @app.errorhandler(InvalidRequest)
     def handle_invalid_usage(error):
         response = jsonify(error.to_dict())
         response.status_code = error.status_code
         return response
 
-    resources = [strategies.resource]
-    for resource in resources:
-        app.register_blueprint(resource)
-
     @app.route('/')
     def root():
         return redirect('/strategies/', code=303)
 
+    resources = [strategies.resource]
+    for resource in resources:
+        app.register_blueprint(resource)
+
     return app
+
+
+def _customize_flask_logger():
+    import logging
+    from zaifbot.logger import bot_logger, bot_file_handler, bot_console_handler
+    logger = logging.getLogger('werkzeug')
+    logger.name = 'Web'
+    logger.setLevel(logging.INFO)
+
+    for hdlr in logger.handlers:
+        logger.removeHandler(hdlr)
+
+    logger.addHandler(bot_console_handler)
+    logger.addHandler(bot_file_handler)
