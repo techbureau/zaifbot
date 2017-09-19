3401663e267a024c81fb0e9ccb9a9e0a541c636a
RossyWhite <daikishiroi@gmail.com>
2017-09-19 17:27:34 +0900

    Clean up code

--- zaifbot/config/handler.py
+++ zaifbot/config/handler.py
@@ -2,11 +2,9 @@ import os
 import configparser
 
 
-def _only_file_exists(func):
-    def _inner(self, *args, **kwargs):
-        if os.path.isfile(self._path):
-            return func(self, *args, **kwargs)
-    return _inner
+def _get_config():
+    config = configparser.ConfigParser()
+    return config
 
 
 class ConfigFileHandler:
@@ -15,9 +13,9 @@ class ConfigFileHandler:
     def __init__(self, *args, **kwargs):
         self._path = kwargs.get('path') or self.default_path
 
-    @_only_file_exists
     def read_by_section(self, section):
-        config = self._get_config()
+        config = _get_config()
+        config.read(self._path)
 
         results = {}
         try:
@@ -27,26 +25,22 @@ class ConfigFileHandler:
         except configparser.NoSectionError:
             return None
 
-    @_only_file_exists
     def read_by_section_and_key(self, section, key):
-        config = self._get_config()
+        config = _get_config()
+        config.read(self._path)
 
         try:
             return config[section][key]
         except (configparser.NoSectionError, KeyError):
             return None
 
-    @_only_file_exists
     def set_config(self, section, key, value):
-        config = self._get_config()
+        config = _get_config()
+        config.read(self._path)
+
         if section not in config.sections():
             config.add_section(section)
         config.set(section, key, value)
 
         with open(self._path, 'w') as configfile:
             config.write(configfile)
-
-    def _get_config(self):
-        config = configparser.ConfigParser()
-        config.read(self._path)
-        return config
